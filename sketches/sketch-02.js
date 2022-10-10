d1c0f71ce56cb394f9975bcfeeb6aa8c3f890031
Zhminko Roman <92923411+ZhminkoRoman@users.noreply.github.com>
2022-10-10 10:10:50 +0300

    feat(creativeCoding l_4): create table sketch

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -2,58 +2,67 @@ const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
 
+
 const settings = {
-  dimensions: [ 1080, 1080 ]
+	dimensions: [ 1080, 1080 ],
+  // animation: true,
+  // duration: 3,
+  // fps: 60,
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+	return ({ context, width, height, playhead }) => {
+		context.fillStyle = 'white';
+		context.fillRect(0, 0, width, height);
 
-    context.fillStyle = 'black';
+		context.fillStyle = 'black';
 
-    const cx = width * 0.5;
-    const cy = height * 0.5;
-    const w = width * 0.01;
-    const h = height * 0.1;
+		const cx = width  * 0.5;
+		const cy = height * 0.5;
+
+		const w = width  * 0.01;
+		const h = height * 0.1;
+
     let x, y;
 
-    const num = 60;
-    const radius = width * 0.9;
+		const num = 40;
+		const radius = width * 0.3;
 
-    for (let i = 0; i < num; i++) {
-      const slice = math.degToRad(360 / num);
-      const angle = slice * i;
+		for (let i = 0; i < num; i++) {
+			const slice = math.degToRad(360 / num);
+			const angle = slice * i;
 
-      x = cx + radius * Math.sin(angle);
-      y = cy + radius * Math.cos(angle);
+			x = cx + radius * Math.sin(angle);
+			y = cy + radius * Math.cos(angle);
 
-      context.save();
-      context.translate(0, 0);
-      context.rotate(-angle);
-      context.scale(random.range(0.5, 5), random.range(0.1, 15.9));
+      draw(context, x, y, w, h, angle, cx, cy, slice, radius);
+		};
 
+	};
 
-      context.beginPath();
-      context.rect(random.range(0, -w / 2), random.range(0, -h * 0.5), w / 6, h);
-      context.fillStyle = `rgb(${4 * i}, ${17 * i}, -${4* i})`;
-      context.fill();
-      context.restore();
+  function draw (context, x, y, w, h, angle, cx, cy, slice, radius) {
+    context.save();
+    context.translate(x, y);
+    context.rotate(-angle);
+    context.scale(2, 0.5);
 
-      context.save();
-      context.translate(0, 0);
-      context.rotate(-angle * 2.5);
+    context.beginPath();
+    context.rect(-w * 0.5, 0.5, w, h);
+    context.fill();
+    context.restore();
 
-      context.lineWidth = random.range(1, 10);
+    context.save();
+    context.translate(cx, cy);
+    context.rotate(-angle);
 
-      context.beginPath();
-      context.arc(-50, -50, radius * random.range(0.1, 3.3), slice * random.range(1, 24), slice * random.range(1, 62));
-      context.strokeStyle = `rgb(${17 * i}, ${4 * i}, ${2 * i})`;
-      context.stroke();
-      context.restore();
-    }
-  };
+    context.lineWidth = random.range(5, 20);
+
+    context.beginPath();
+    context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
+    context.stroke();
+
+    context.restore();
+  }
 };
 
 canvasSketch(sketch, settings);
--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -24,7 +24,7 @@ const sketch = ({ context, width, height }) => {
   }
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = '#111111';
     context.fillRect(0, 0, width, height);
 
     for (let i = 0; i < agents.length; i++) {
@@ -35,13 +35,68 @@ const sketch = ({ context, width, height }) => {
 
         const dist = agent.pos.getDistance(other.pos);
 
+        if (dist <= agent.radius + other.radius) {
+          if (agent.vel.y * other.vel.y > 0 && agent.vel.x * other.vel.x > 0) {
+            if (Math.abs(agent.pos.x) > Math.abs(other.pos.x) && Math.abs(agent.pos.y) > Math.abs(other.pos.y)) {
+              if (agent.vel.x < other.vel.x && agent.vel.y < other.vel.y) {
+                other.vel.x *= -1;
+                other.vel.y *= -1;
+              } else if (agent.vel.x < other.vel.x && agent.vel.y > other.vel.y) {
+                other.vel.x *= -1;
+                other.vel.y *= -1;
+              } else if (agent.vel.x > other.vel.x && agent.vel.y < other.vel.y) {
+                other.vel.x *= -1;
+                other.vel.y *= -1;
+              }
+            } else if (Math.abs(agent.pos.x) < Math.abs(other.pos.x) && Math.abs(agent.pos.y) < Math.abs(other.pos.y)) {
+              if (agent.vel.x > other.vel.x && agent.vel.y > other.vel.y) {
+                agent.vel.x *= -1;
+                agent.vel.y *= -1;
+              } else if (agent.vel.x < other.vel.x && agent.vel.y > other.vel.y) {
+                agent.vel.x *= -1;
+                agent.vel.y *= -1;
+              } else if (agent.vel.x > other.vel.x && agent.vel.y < other.vel.y) {
+                agent.vel.x *= -1;
+                agent.vel.y *= -1;
+              }
+            } else if (Math.abs(agent.pos.x) < Math.abs(other.pos.x) && Math.abs(agent.pos.y) > Math.abs(other.pos.y)) {
+              agent.vel.y *= -1;
+              other.vel.y *= -1;
+              agent.vel.x *= -1;
+              other.vel.x *= -1;
+            } else if (Math.abs(agent.pos.x) > Math.abs(other.pos.x) && Math.abs(agent.pos.y) < Math.abs(other.pos.y)) {
+              agent.vel.y *= -1;
+              other.vel.y *= -1;
+              agent.vel.x *= -1;
+              other.vel.x *= -1;
+            }
+          } else if (agent.vel.x * other.vel.x > 0) {
+            agent.vel.y *= -1;
+            other.vel.y *= -1;
+          } else if (agent.vel.y * other.vel.y > 0) {
+            agent.vel.x *= -1;
+            other.vel.x *= -1;
+          } else {
+            agent.vel.y *= -1;
+            other.vel.y *= -1;
+            agent.vel.x *= -1;
+            other.vel.x *= -1;
+          }
+        }
+
+        if (dist => 0 && dist <= 200) {
+          // agent.strokeStyle = `rgb(${255 - Math.round(dist)}, 49, 49)`;
+          // other.strokeStyle = `rgb(${255 - Math.round(dist)}, 49, 49)`;
+        }
+
         if (dist > 200) continue;
 
-        context.lineWidth = math.mapRange(dist, 0, 200, 12, 1);
+        context.lineWidth = math.mapRange(dist, 0, 200, 6, 1);
 
         context.beginPath();
         context.moveTo(agent.pos.x, agent.pos.y);
         context.lineTo(other.pos.x, other.pos.y);
+        context.strokeStyle = `rgba(255, 255, 255, 0.5)`;
         context.stroke();
       }
     }
@@ -52,6 +107,7 @@ const sketch = ({ context, width, height }) => {
       agent.bounce(width, height);
     });
   };
+  
 };
 
 canvasSketch(sketch, settings);
@@ -75,6 +131,22 @@ class Agent {
     this.pos = new Vector(x, y);
     this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
     this.radius = random.range(4, 12);
+    this.strokeStyle = `rgba(255, 255, 255, 0.5)`;
+  }
+
+  wrap(width, height) {
+    if (this.pos.x > width) {
+      this.pos.x = 0; 
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width; 
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0; 
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height; 
+    }
   }
 
   bounce(width, height) {
@@ -100,7 +172,9 @@ class Agent {
 
     context.beginPath();
     context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.fillStyle = 'white';
     context.fill();
+    context.strokeStyle = this.strokeStyle;
     context.stroke();
 
     context.restore();
